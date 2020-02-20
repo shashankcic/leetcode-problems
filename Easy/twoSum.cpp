40242eb3eb4c8730abf572025f20a109b7abca0c
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-02-20 22:26:50 +0530

    Made required changes to the twoSum file

--- Easy/twoSum.cpp
+++ Easy/twoSum.cpp
@@ -1,77 +1,54 @@
-#include <vector>
-#include <bits/stdc++.h>
-
-using namespace std;
-
-vector<int> twoSum(vector<int>& nums, int target) {
-    short int start=0,end=nums.size()-1,check=0;
-    int temp[end+1];
-    for(short int i=start;i<=end;i++){
-        temp[i]=nums[i];
-    }
-    sort(temp,temp+end+1);
-    for(short int i=start;i<=end;i++)
-    {
-        for(short int j=start;j<=end;j++)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        short int start=0,end=nums.size()-1,check=0;
+        int temp[end+1];
+        for(short int i=start;i<=end;i++){
+            temp[i]=nums[i];
+        }
+        sort(temp,temp+end+1);
+        for(short int i=start;i<=end;i++)
         {
-            if(i!=j)
-                if(temp[i]+temp[j]==target)
-                {
-                    start=i;
-                    end=j;
-                    check=1;
-                    cout<<1<<check<<" "<<start<<" "<<end<<" "<<endl;
-                    break;
-                }
+            for(short int j=i+1;j<=end;j++)
+            {
+                if(i!=j)
+                    if(temp[i]+temp[j]==target)
+                    {
+                        start=i;
+                        end=j;
+                        check=1;
+                        break;
+                    }
+            }
+            if(check==1)
+                break;
         }
         if(check==1)
-            break;
-    }
-    if(check==1)
-    {
-        for(int i=0;i<nums.size()-1;i++)
         {
-            if(nums.at(i)==temp[start])
+            for(short int i=0;i<nums.size();i++)
             {
-                start=i;
-                check=2;
-                cout<<2<<check<<" "<<start<<" "<<end<<" "<<endl;
-                break;
+                if(nums.at(i)==temp[start])
+                {
+                    start=i;
+                    check=2;
+                    break;
+                }
+                if(check==2)
+                    break;
             }
-            if(check==2)
-                break;    
-        }
-        cout<<3<<check<<" "<<start<<" "<<end<<" "<<endl;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(i!=start)
-                if(nums.at(i)==temp[end])
+            for(short int i=0;i<nums.size();i++)
+            {
+                if(nums.at(i)==temp[end]&&start!=i)
                 {
                     end=i;
                     check=3;
-                    cout<<4<<check<<" "<<start<<" "<<end<<" "<<endl;
                     break;
                 }
-            if(check==3)
-                break;    
-        }
-        cout<<5<<check<<" "<<start<<" "<<end<<" "<<endl;
-        //if(check==3)
-        {
+                if(check==3)
+                    break;
+            }
             return {start,end};
         }
+        return nums;
     }
-    cout<<check<<" "<<start<<" "<<end<<" "<<endl;
-    return nums;
-}
-
-int main(){
-    vector<int> v= {0,4,3,0};
-    int target = 0;
-   vector<int> result=twoSum(v,target);
-    
-
-    for(auto i=result.begin();i!=result.end();i++)
-        cout<<*i<<endl;
-
-}
+};
